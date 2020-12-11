98bde52cd67b2ce3c3a203948f25f761f0f1cc73
root <root@Edwin-PC.localdomain>
2020-12-11 19:48:36 +0800

    fixed error on wrong views generate.

--- test/controllers/entries_controller_test.rb
+++ test/controllers/entries_controller_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class EntriesControllerTest < ActionDispatch::IntegrationTest
 
     setup do
+        # @user = users(:one)
+        # post new_user_session_path
         @entry = entries(:default)
         @category = categories(:two)
     end
@@ -18,11 +20,13 @@ class EntriesControllerTest < ActionDispatch::IntegrationTest
     end 
 
     test "should create entry" do
-        assert_difference('Entry.count') do
-          post category_entries_path(@category, @entry), params: { entry: {name: 'task one', details: 'must do this'} }
+        post category_entries_path(@category, @entry), params: { entry: {name: 'task one', details: 'must do this', category_id: 5} }
+        
+        assert_difference 'Entry.count', 1 do
+        assert_response :success
         end
-    
+
         assert_redirected_to category_entries_path
-      end
+    end
 
 end
